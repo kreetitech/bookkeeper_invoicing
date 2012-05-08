908035c8324202fc6684f43005157c6fbe3f62e0
Santanu Karmakar <skarmakar@kreeti.com>
2012-05-08 11:46:24 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookkeeperInvoicing::Application.config.secret_token = '69a1b1a4b05eacbeb8ecca2108a1d9c40766b9f6133f9a2d32f55ea38749d8bedc3c8a6150924a987e711527856c0be08bfc66d690995ece8edfe5cb3cb45716'
